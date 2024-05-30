144ec36246c18a8b07e03e0b0e80124f96b4efd6
MrSurenK <129926758+MrSurenK@users.noreply.github.com>
2024-05-30 17:34:31 +0800

    Set up error handler.

--- futsalbot.py
+++ futsalbot.py
@@ -1,17 +1,40 @@
+import logging
 import os
 from dotenv import load_dotenv, dotenv_values
 
-import asyncio
-import telegram
-
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 load_dotenv()
 
-async def main():
-    bot = telegram.Bot(os.getenv("API_KEY"))
-    async with bot:
-        print(await bot.get_me())
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+session_info = {
+    "date": " ",
+    "time": " ",
+    "location": " ",
+    "players": {},
+    "paid": []
+}
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'll help you organise your futsal session!")
+
+# aysnc def organise(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv("API_KEY")).build()
+
+    # Must be last handler to handle wrong commands
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+
+    # Must be last handler in application instance
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
 
